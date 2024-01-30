a5c659a26bf992da7c371fbfd534e391ea4d8aca
breath03 <baekjimin0301@gmail.com>
2024-01-31 02:44:49 +0900

    2024-01-30

--- js/main.js
+++ js/main.js
@@ -100,3 +100,17 @@ infoEls.forEach(function(el) {
 
 
 // 비디오 재생!
+const video = document.querySelector('.stage video')
+const playBtn = document.querySelector('.stage .controller--play')
+const pauseBtn = document.querySelector('.stage .controller--pause')
+
+playBtn.addEventListener('click', function() {
+  video.play()
+  playBtn.classList.add('hide')
+  pauseBtn.classList.remove('hide')
+})
+pauseBtn.addEventListener('click', function() {
+  video.pause()
+  playBtn.classList.remove('hide')
+  pauseBtn.classList.add('hide')
+})
