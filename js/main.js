30640f65a4d8d895adca69b0112ac9bb11194a8b
breath03 <baekjimin0301@gmail.com>
2024-02-04 03:39:21 +0900

    2024-02-03

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ searchShadowEl.addEventListener('click', hideSearch);
 
 function showSearch() {
   headerEl.classList.add('searching');
-  document.documentElement.classList.add('fixed');
+  stopScroll()
   headerMenuEls.reverse().forEach(function(el, index) {
     el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
   });
@@ -72,7 +72,7 @@ function showSearch() {
 };
 function hideSearch() {
   headerEl.classList.remove('searching');
-  document.documentElement.classList.remove('fixed');
+  playScroll()
   headerMenuEls.reverse().forEach(function(el, index) {
     el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
   });
@@ -83,6 +83,27 @@ function hideSearch() {
   // 해당 input의 내용에 입력했던 값을 지워주기!
   searchInputEl.value = '';
 };
+function playScroll() {
+  document.documentElement.classList.remove('fixed');
+}
+function stopScroll() {
+  document.documentElement.classList.add('fixed');
+}
+
+
+
+
+// 헤더 메뉴 토글!
+const menuStarterEl = document.querySelector('header .menu-starter')
+menuStarterEl.addEventListener('click', function() {
+  if (headerEl.classList.contains('menuing')) {
+    headerEl.classList.remove('menuing')
+    playScroll()
+  } else {
+    headerEl.classList.add('menuing')
+    stopScroll()
+  }
+})
 
 
 
